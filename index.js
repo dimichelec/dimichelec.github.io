1d173e1763f5c0f1d7ae1c232ee53226f3d7fec6
dimichelec <dimichelec@gmail.com>
2019-05-04 15:37:48 -0400

    x

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
-const yaml = require('js-yaml')
-const fs = require('fs')
+const express = require('express');
+const path    = require('path');
+const yaml    = require('js-yaml');
+const fs      = require('fs');
+const crypto  = require('crypto');
+const PORT    = process.env.PORT || 5000;
 
-var settings = yaml.load(
-	fs.readFileSync(
-		path.join(__dirname, 'public', 'dk-rotator.yml'),
-		'utf8'
-	)
-)
 
+// calculate and return current hash of settings file
+function settingsHash() {
+	var hash = crypto
+		.createHash('md5')
+		.setEncoding('hex');
+	hash.write(
+		fs.readFileSync(
+			path.join(__dirname, 'public', 'dk-rotator.yml'),
+			'utf8'
+		)
+	);
+	hash.end();
+	return hash.read();
+}
+
+// read and return current settings
+function settings() {
+	return yaml.load(
+		fs.readFileSync(
+			path.join(__dirname, 'public', 'dk-rotator.yml'),
+			'utf8'
+		)
+	);
+}
+
+
+// Setup Server
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index', {settings: settings}))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+  // root handler
+  .get('/', (req, res) => res.render('pages/index', {hash: settingsHash(), settings: settings()}))
+  
+  // /getSettingsHas handler
+  .get('/getSettingsHash', function(req, res) {	res.send({hash: settingsHash()}); })
+  
+  // /getSettings handler
+  .get('/getSettings', (req, res) => res.send({hash:settingsHash(), settings: settings()}))
+  
+  // Start server
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+

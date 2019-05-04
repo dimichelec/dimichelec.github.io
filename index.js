8b53727a59ba15eeaa97b249258b55bb68f64f13
dimichelec <dimichelec@gmail.com>
2019-05-04 10:57:00 -0400

    x

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var yaml = require('js-yaml')
+var fs = require('fs')
+var doc = yaml.load(fs.readFile('dk-rotator.yaml','utf8'))
+
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
